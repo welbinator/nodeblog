018eb2ba06b1c5d87375e388211a057f11b8e917
James Welbes <james.welbes@gmail.com>
2020-06-02 12:40:29 -0500

    regular commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const expressSanitizer = require("express-sanitizer");
 const mongoose = require("mongoose");
 
 // GETS RID OF SOME DEPRECATION ERRORS
@@ -21,7 +23,8 @@ mongoose.connection.on('error', err => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 // MONGOOSE/MODEL CONFIG
 const blogSchema = new mongoose.Schema({
@@ -57,6 +60,7 @@ app.get("/blogs/new", (req, res) => {
 
 //CREATE ROUTE
 app.post("/blogs", (req, res) => {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, (err, newBlog) => {
         if (err) {
             res.render("new");
@@ -77,7 +81,44 @@ app.get("/blogs/:id", (req, res) => {
     })
 })
 
+//EDIT ROUTE
+app.get("/blogs/:id/edit", (req, res) => {
 
+    Blog.findById(req.params.id, (err, foundBlog) => {
+        if (err) {
+            res.redirect("/blogs")
+        } else {
+            res.render("edit", { blog: foundBlog })
+        }
+    })
+
+})
+
+//UPDATE ROUTE
+app.put("/blogs/:id", (req, res) => {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    })
+})
+
+//DELETE ROUTE
+
+app.delete("/blogs/:id", (req, res) => {
+    Blog.findByIdAndRemove(req.params.id, (err) => {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    })
+})
+
+//SERVER RUNNING
 app.listen(3001, "100.115.92.196", () => {
     console.log("Server is running");
 });
